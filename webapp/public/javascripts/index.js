213611da510c59e1c3d551b25a11de271d8f214b
Daniel Gorelick <dqgorelick@gmail.com>
2016-02-20 14:04:49 -0500

    styling for map, and directional train markers

---
+++ webapp/public/javascripts/index.js
@@ -0,0 +1,94 @@
+"use strict";
+
+function loadJSON(option, callback) {
+  var path;
+  switch(option) {
+    case 'positions':
+      path = './data/positions.json'
+      break;
+    case 'lines' :
+      path = './data/mbta_lines.json'
+      break;
+    default :
+      console.log('no option selected');
+      return;
+  }
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', path, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+function getColor(line) {
+  switch(line.toLowerCase().slice(0,3)) {
+    case 'gre':
+      return '#357F4C';
+      break;
+    case 'red':
+      return '#F03911';
+      break;
+    case 'blu':
+      return '#295CAB';
+      break;
+    case 'ora':
+      return '#f08f00';
+      break;
+    default:
+      // set all other lines to 0 opacity
+      return 'rgba(255,255,255,0)';
+      break;
+  }
+}
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.3601, lng:  -71.1},
+    zoom: 13
+  });
+  // map styles
+  map.setOptions({styles: styles});
+  map.OverviewMapControlOptions = false;
+
+  // change this to local eventually
+  map.data.loadGeoJson("https://raw.githubusercontent.com/BU-NU-CLOUD-SP16/MBTA-Alerts-and-Performance-Anlaysis/master/webapp/public/data/mbta_lines.json");
+  map.data.setStyle(function(feature) {
+    if (feature.R.LINE) {
+      var color = getColor(feature.R.LINE);
+    }
+    return ({
+      strokeColor: color,
+      strokeWeight: 5,
+      strokeOpacity : 0.5
+    });
+  });
+
+  // load positions from 2/19/16 around 3PM
+  loadJSON('positions', function(response) {
+    var positions = JSON.parse(response);
+    positions.data.forEach(function(item){
+      var color = getColor(item.route_id);
+      var icon = {
+          // path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW, <- trains might be backwards
+          path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+          scale: 3,
+          strokeColor: color,
+          rotation: item.position.bearing
+        };
+      var lat = item.position.latitude;
+      var lng = item.position.longitude;
+      var marker = new google.maps.Marker({
+        position: {lat,lng},
+        map: map,
+        title: item.id,
+        icon : icon
+      });
+    })
+  });
+}
