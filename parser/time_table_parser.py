62257a85f66ce5290705cd4af4034b6a49f9574e
EJ Fitzpatrick <EJ@wireless1x-155-41-122-46.bu.edu>
2016-04-11 21:37:36 -0400

    modified time_table_parser.py to hold averages of 30 minute slices and add dayOfWeek. Also modified time_table to accept dayOfWeek

--- parser/time_table_parser.py
+++ parser/time_table_parser.py
@@ -4,10 +4,11 @@ import json
 import sqlite3 as lite
 import sys
 import time
+import datetime
 
 
 queryTime = int(time.time())
-count = 0
+dayOfWeek = datetime.datetime.today().weekday() #0 = Monday 6 = Sunday
 
 #load api and key from json file
 with open('../auth/api.json') as data_file:
@@ -27,18 +28,14 @@ endTime = queryTime
 #Date from docs example
 startTime = endTime - 30*60
 
-#drop old table
-try:
-    cur.execute('DROP TABLE time_table')
-except lite.Error as e:
-    print("Error")
-
 #create new table
-cur.execute('CREATE TABLE time_table(time integer, line text, direction, integer, stop_id integer, headway integer);')
+#'CREATE TABLE time_table(time integer, line text, direction integer, stop_id integer, headway integer);')
 
 
 #iterate through all stops and insert headway info into database
 for stop in all_stops[0:5]:
+    count = 0
+    headwaySum = 0
 
     params = {
      'api_key' : api_key,
@@ -50,39 +47,36 @@ for stop in all_stops[0:5]:
     encoded_params = urllib.urlencode(params)
 
     url = api + encoded_params
-    print(count)
     
     # reads data from the API endpoint
     result = urllib2.urlopen(url).read()
     
     # Database stuff starts here
-
-    try:
-        headways = json.loads(result)
-        for headway in headways['headways']: #headways_file['headways']: #dive into individual headways
+    headways = json.loads(result)
+    if headways['headways']:
+        try:
             table_time = int(queryTime) #time based on query time
             table_stop_id = int(stop[0])
-            table_direction = int(headway['direction']) #direction from headway.json
-            table_headway = int(headway['headway_time_sec']) #headway interval from headway.json
-            #table_current_dep = int(current_dep['current_dep_dt'])
-            table_route_id = str(headway['route_id'])
+            table_direction = int(headways['headways'][0]['direction']) #direction from headway.json
+            table_route_id = str(headways['headways'][0]['route_id'])
+            for headway in headways['headways']: #headways_file['headways']: #dive into individual headways
+                headwaySum += int(headway['headway_time_sec']) #headway interval from headway.json
+                count += 1
 
-            to_insert = (table_time, table_route_id, table_direction, table_stop_id, table_headway)
-            cur.execute('INSERT INTO time_table(time , line , direction , stop_id , headway) VALUES (?, ?, ?, ?, ?)', to_insert)      
+            headwayAvg = headwaySum/count
+            to_insert = (table_time, table_route_id, table_direction, table_stop_id, headwayAvg, dayOfWeek)
+            cur.execute('INSERT INTO time_table(time , line , direction , stop_id , headway, dayOfWeek) VALUES (?, ?, ?, ?, ?, ?)', to_insert)      
 
-        con.commit()
+            con.commit()
 
-    except lite.Error, e:
+        except lite.Error, e:
 
-        print "Error %s:" % e.args[0]
-        sys.exit(1)
-
-
-        if con:
-            con.close()
-    count = count + 1
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
 
 
+            if con:
+                con.close()
 
 
 
