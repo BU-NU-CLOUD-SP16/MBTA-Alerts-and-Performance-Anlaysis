1dfe3de37c80586f6b83542c893bfcde31dc5b3b
Daniel Gorelick <dqgorelick@gmail.com>
2016-04-19 16:14:41 -0400

    working db queried from the FE

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,196 +1,187 @@
 "use strict";
 
-var showLines = true;
-var showStops = false;
-var showPositions = false;
-var showDwells = true;
-var dwells_0830 = true;
-var dwells_1200 = false;
-var dwells_1730 = false;
-
-function loadJSON(option, callback) {
-  var path;
-  switch(option) {
-    case 'positions':
-      path = './data/positions.json';
-      break;
-    case 'lines' :
-      path = './data/mbta_lines.json';
-      break;
-    case 'stops' :
-      path = './data/stops.json';
-      break;
-    case 'dwells_0830' :
-      path = './data/stops_with_dwell_indicator_1458052607.json';
-      break;
-    case 'dwells_1200' :
-      path = './data/stops_with_dwell_indicator_1458052860.json';
-      break;
-    case 'dwells_1730' :
-      path = './data/stops_with_dwell_indicator_1458052742.json';
-      break;
-    default :
-      console.log('no option selected');
-      return;
-  }
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', path, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+var options = {
+    lines: true,
+    stops: false,
+    positions: false,
+    dwells: false,
+    line: false
 }
 
-function getColor(line) {
-  switch(line.toLowerCase().slice(0,3)) {
-    // case 'gre':
-    //   return '#357F4C';
-    //   break;
-    case 'red':
-      return '#F03911';
-      break;
-    // case 'blu':
-    //   return '#295CAB';
-    //   break;
-    // case 'ora':
-    //   return '#f08f00';
-    //   break;
-    default:
-      // set all other lines to 0 opacity
-      return 'rgba(255,255,255,0)';
-      break;
-  }
+function load_json(path, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function get_color(line) {
+    switch (line.toLowerCase().slice(0, 3)) {
+        case 'gre':
+            return (options.line == 'green' ? '#357F4C' : 'rgba(255,255,255,0)');
+            break;
+        case 'red':
+            return (options.line == 'red' ? '#F03911' : 'rgba(255,255,255,0)');
+            break;
+        case 'blu':
+            return (options.line == 'blue' ? '#295CAB' : 'rgba(255,255,255,0)');
+            break;
+        case 'ora':
+            return (options.line == 'orange' ? '#f08f00' : 'rgba(255,255,255,0)');
+            break;
+        default:
+            return 'rgba(255,255,255,0)';
+            break;
+    }
 }
 
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.3601, lng:  -71.1},
-    zoom: 13
-  });
-  // map styles
-  map.setOptions({styles: styles});
-  map.OverviewMapControlOptions = false;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 42.3601, lng: -71.1 },
+        zoom: 11
+    });
+    // map styles
+    map.setOptions({ styles: styles });
 
-  if(showLines) {
-    // change this to local eventually
-    map.data.loadGeoJson("https://raw.githubusercontent.com/BU-NU-CLOUD-SP16/MBTA-Alerts-and-Performance-Anlaysis/master/webapp/public/data/mbta_lines.json");
-    map.data.setStyle(function(feature) {
-      if (feature.R.LINE) {
-        var color = getColor(feature.R.LINE);
-      }
-      return ({
-        strokeColor: color,
-        strokeWeight: 5,
-        strokeOpacity : 0.5
-      });
-    });
-  }
-/*
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('legend'));
-  var legend = document.getElementById('legend');
-  var div = document.createElement('div');
-  div.innerHTML = '<p><img src="../assets/stop.png"> Train Stop </p>';
-  legend.appendChild(div);
-*/
-  if(showDwells) {
-    loadJSON('dwells_0830', function(response) {
-      var data = JSON.parse(response);
-      for (var stop in data.stops) {
-          var color = "#333333";
-          var indicator = parseFloat(data.stops[stop].indicator);
-          if (indicator === NaN) indicator = 0.02;
-          var icon = {
-              path: google.maps.SymbolPath.CIRCLE,
-			  labelContent: indicator,
-              scale: indicator * 100,
-              fillColor: color,
-              fillOpacity: 0.5,
-              strokeOpacity: 0
-            };
-          var lat = parseFloat(data.stops[stop].station.stop_lat);
-          var lng = parseFloat(data.stops[stop].station.stop_lon);
-		  
-		  var marker = new google.maps.Marker({
-            position: {lat,lng},
-            map: map,
-            title: data.stops[stop].station.stop_name,
-            icon : icon
-          });
-		  var infowindow = new google.maps.InfoWindow({
-		    content: '<p>' + data.stops[stop].indicator + '</p>'
-		  });
-      }
-        // var infowindow = new google.maps.InfoWindow({
-        //   content: '<p>Stop: '+item.stop_name+'</p>'
-        // });
-        // marker.addListener('click', function() {
-        //   infowindow.open(map, marker);
-        // });
-    })
-  }
-  if(showStops) {
-    loadJSON('stops', function(response) {
-      var data = JSON.parse(response);
-      data.stops.forEach(function(item){
-        var color = "#333333";
-        var icon = {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 4,
-            fillColor: color,
-            fillOpacity: 0.5,
-            strokeOpacity: 0
-          };
-        var lat = item.stop_lat;
-        var lng = item.stop_lon;
-        var infowindow = new google.maps.InfoWindow({
-          content: '<p>Stop: '+item.stop_name+'</p>'
+    if (options.lines) {
+        // change this to local eventually
+        map.data.loadGeoJson("https://raw.githubusercontent.com/BU-NU-CLOUD-SP16/MBTA-Alerts-and-Performance-Anlaysis/master/webapp/public/data/mbta_lines.json");
+        map.data.setStyle(function(feature) {
+            if (feature.R.LINE) {
+                var color = get_color(feature.R.LINE);
+            }
+            return ({
+                strokeColor: color,
+                strokeWeight: 5,
+                strokeOpacity: 0.5
+            });
         });
-        var marker = new google.maps.Marker({
-          position: {lat,lng},
-          map: map,
-          title: item.stop_name,
-          icon : icon
+    }
+    if (options.dwells) {
+        load_json('./data/stops_with_dwell_indicator_1458052607.json', function(response) {
+            var data = JSON.parse(response);
+            for (var stop in data.stops) {
+                var color = "#333333";
+                var indicator = parseFloat(data.stops[stop].indicator);
+                if (indicator === NaN) indicator = 0.02;
+                var icon = {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    labelContent: indicator,
+                    scale: indicator * 100,
+                    fillColor: color,
+                    fillOpacity: 0.5,
+                    strokeOpacity: 0
+                };
+                var lat = parseFloat(data.stops[stop].station.stop_lat);
+                var lng = parseFloat(data.stops[stop].station.stop_lon);
+
+                var marker = new google.maps.Marker({
+                    position: { lat, lng },
+                    map: map,
+                    title: data.stops[stop].station.stop_name,
+                    icon: icon
+                });
+            }
+        })
+    }
+    if (options.stops) {
+        load_json('./data/stops.json', function(response) {
+            var data = JSON.parse(response);
+            data.stops.forEach(function(item) {
+                var color = "#333333";
+                var icon = {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    scale: 4,
+                    fillColor: color,
+                    fillOpacity: 0.5,
+                    strokeOpacity: 0
+                };
+                var lat = item.stop_lat;
+                var lng = item.stop_lon;
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<p>Stop: ' + item.stop_name + '</p>'
+                });
+                var marker = new google.maps.Marker({
+                    position: { lat, lng },
+                    map: map,
+                    title: item.stop_name,
+                    icon: icon
+                });
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                });
+            })
         });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
+    }
+
+    if (options.positions) {
+        // load positions from 2/19/16 around 3PM
+        load_json('./data/positions.json', function(response) {
+            var positions = JSON.parse(response);
+            positions.data.forEach(function(item) {
+                var color = get_color(item.route_id);
+                var icon = {
+                    // path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW, <- trains might be backwards
+                    path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+                    scale: 3,
+                    strokeColor: color,
+                    rotation: item.position.bearing
+                };
+                var lat = item.position.latitude;
+                var lng = item.position.longitude;
+                var contentString = '<p>' + item.route_id + ' Line</p>' + '<p>' + item.id + ' at stop ' + item.stop_id + '</p>';
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                var marker = new google.maps.Marker({
+                    position: { lat, lng },
+                    map: map,
+                    title: item.id,
+                    icon: icon
+                });
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                });
+            })
         });
-      })
-    });
-  }
-  if(showPositions) {
-    // load positions from 2/19/16 around 3PM
-    loadJSON('positions', function(response) {
-      var positions = JSON.parse(response);
-      positions.data.forEach(function(item){
-        var color = getColor(item.route_id);
-        var icon = {
-            // path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW, <- trains might be backwards
-            path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
-            scale: 3,
-            strokeColor: color,
-            rotation: item.position.bearing
-          };
-        var lat = item.position.latitude;
-        var lng = item.position.longitude;
-    	  var contentString = '<p>'+item.route_id+' Line</p>'+'<p>'+item.id+' at stop '+item.stop_id+'</p>';
-    	  var infowindow = new google.maps.InfoWindow({
-    		  content: contentString
-    	  });
-        var marker = new google.maps.Marker({
-          position: {lat,lng},
-          map: map,
-          title: item.id,
-          icon : icon
-        });
-    	  marker.addListener('click', function() {
-    		  infowindow.open(map, marker);
-    	  });
-      })
-    });
-  }
+    }
 }
+
+$(".red-line").on("click", function() {
+  options.line = 'red';
+  initMap(options);
+  load_json('http://localhost:8080/api/mbta/headways/Red', function(response){
+    $(".data-container").html(response);
+  });
+})
+
+$(".green-line").on("click", function() {
+  options.line = 'green';
+  initMap(options);
+  load_json('http://localhost:8080/api/mbta/headways/Green', function(response){
+    $(".data-container").html(response);
+  });
+})
+
+$(".blue-line").on("click", function() {
+  options.line = 'blue';
+  initMap(options);
+  load_json('http://localhost:8080/api/mbta/headways/Blue', function(response){
+    $(".data-container").html(response);
+  });
+})
+
+$(".orange-line").on("click", function() {
+  options.line = 'orange';
+  initMap(options);
+  load_json('http://localhost:8080/api/mbta/headways/Orange', function(response){
+    $(".data-container").html(response);
+  });
+})
--- webapp/server.js
+++ webapp/server.js
@@ -13,6 +13,12 @@ var sqlite = require("sqlite3").verbose();
 var mbta = new sqlite.Database("../parser/mbta_subway.db");
 
 app.use(compression()); // compress data being sent
+
+
+// simple caching
+var all_lines = false;
+all_lines = get_all_lines();
+
 router.use(function(req, res, next) {
     console.log('Request made!');
     next();
@@ -28,29 +34,53 @@ router.route('/line/:line').get(function(req, res) {
                 console.log(err);
             } else {
                 console.log(row);
-                res.json({message: row});
+                res.json({ message: row });
             }
         }
     );
 });
 
 router.route('/all').get(function(req, res) {
-    mbta.all(
-        "SELECT * FROM Static",
-        function(err, row) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(row);
-                res.json({message: row});
-            }
-        }
-    );
+    if (all_lines) {
+        console.log("Sending cached version");
+        return res.json({ message: all_lines });
+    } else {
+        console.log("Sending DB version");
+        mbta.all("SELECT * FROM Static", function(err, row) {
+            res.json(err ? false : row);
+            all_lines = (err ? false : row);
+        });
+    }
 });
+
+
+function get_all_lines() {
+    mbta.all("SELECT * FROM Static", function(err, row) {
+        return (err ? false : row);
+    });
+}
+
+
+// get most recent query
+router.route('/headways/:line').get(function(req, res) {
+    // mbta.all("SELECT * FROM time_table WHERE time = (SELECT MAX(time) FROM time_table) AND line = $line", {
+    if(req.params.line == 'Green') {
+        mbta.all("SELECT * FROM time_table WHERE time = 1460733601 AND (line = 'Green-E' OR line == 'Green-B' OR line == 'Green-D' OR line == 'Green-C')",
+            function(err, row) {
+                res.json(err ? false : row);
+            });
+    } else {
+        mbta.all("SELECT * FROM time_table WHERE time = 1460733601 AND line = $line", {
+            $line: req.params.line
+        }, function(err, row) {
+            res.json(err ? false : row);
+        });
+    }
+})
 
 app.use('/api/mbta', router);
 app.set('port', port);
 app.use(express.static("./public"));
 http.createServer(app).listen(app.get('port'), function() {
- console.log('HTTP server listening on PORT ' + app.get('port'));
+    console.log('HTTP server listening on PORT ' + app.get('port'));
 });
